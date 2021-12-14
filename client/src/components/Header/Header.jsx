33eeef95346b7a83e54b9155b317b95fbcd7e421
AntoLeRigoloo <antoine.maes@outlook.fr>
2021-12-14 16:57:10 +0100

    Added Header languages

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,66 @@
+import styles from "./Header.module.scss";
+import enFlag from "../../assets/images/Angleterre.png";
+import esFlag from "../../assets/images/Espagne.png";
+import frFlag from "../../assets/images/France.png";
+
+import { ReactComponent as Arrow } from "../../assets/icons/arrow.svg";
+
+
+import { useState, useEffect } from "react";
+
+const Header = () => {
+
+  const [langChecked, setLangChecked] = useState(false);
+  const [navChecked, setNavChecked]= useState(false);
+
+  return (
+    <div className={styles.headerContainer}>
+      <header className={styles.header}>
+        <div className={styles.logo}>MyTravelBuddy</div>
+
+        <div className={styles.rightHeader}>
+          <span className={styles.donate}>Faire un don</span>
+          <label htmlFor="showLanguage" className={styles.countriesSelection} onClick={() => setLangChecked(!langChecked)}>
+            <div className={`${styles.lang} ${langChecked && styles.rotate}`}>
+              <img className={styles.flag} src={frFlag} />
+              <span>FR</span>
+              <Arrow />
+            </div>
+          </label>
+
+          <label htmlFor="showNav" className={styles.ok} onClick={()=>setNavChecked(!navChecked)}>
+            <div className={`${styles.hamburger} ${navChecked && styles.change}`}>
+              <div className={styles.hamburgerLine}></div>
+              <div className={styles.hamburgerLine}></div>
+              <div className={styles.hamburgerLine}></div>
+            </div>
+          </label>
+        </div>
+      </header>
+      <input
+        type="checkbox"
+        id="showLanguage"
+        className={styles.LanguagesInput}
+      />
+      <div className={styles.Languages}>
+        <div className={styles.choiceLanguage}>
+          <img className={styles.flag} src={frFlag} />
+          <span>FR</span>
+        </div>
+        <div className={styles.choiceLanguage}>
+          <img className={styles.flag} src={enFlag} />
+          <span>EN</span>
+        </div>
+        <div className={styles.choiceLanguage}>
+          <img className={styles.flag} src={esFlag} />
+          <span>ES</span>
+        </div>
+      </div>
+
+      <input type="checkbox" id="showNav" className={styles.navHeaderInput} />
+      <div className={styles.navHeader}>NAV</div>
+    </div>
+  );
+};
+
+export default Header;
